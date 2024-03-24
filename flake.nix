3f610b96f9eb8f2d81cb4034d3571ee9b0905d7b
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-24 08:21:06 -0400

    move nix-darwin into hosts directory

--- flake.nix
+++ flake.nix
@@ -3,48 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [
-        fish
-      ];
-
-      environment.shells = [ pkgs.fish ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
+  outputs = inputs@{ self, darwin, nixpkgs, home-manager }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Sloanes-MacBook-Air
-    darwinConfigurations."Sloanes-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations."Sloanes-MacBook-Air" = darwin.lib.darwinSystem {
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      modules = [ 
+        ./hosts/Sloanes-MacBook-Air/default.nix
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
