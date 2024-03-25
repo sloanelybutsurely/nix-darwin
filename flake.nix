cbc0e05397f1d5591f8fa3bcd3917bcb08d5280f
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-25 10:12:18 -0400

    copy nix-darwin config to Sloanes-MacBook-Pro
    would be better to share this some other way...

--- flake.nix
+++ flake.nix
@@ -35,7 +35,26 @@
       ];
     };
 
+    darwinConfigurations."Sloanes-MacBook-Pro" = darwin.lib.darwinSystem {
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      modules = [ 
+        ./hosts/Sloanes-MacBook-Pro/default.nix
+        nixvim.nixDarwinModules.nixvim
+        home-manager.darwinModules.home-manager
+        {
+          users.users.sloane.home = "/Users/sloane";
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.sloane = import ./users/sloane/default.nix;
+          };
+        }
+      ];
+    };
+
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Sloanes-MacBook-Air".pkgs;
+    # darwinPackages = self.darwinConfigurations."Sloanes-MacBook-Air".pkgs;
   };
 }
